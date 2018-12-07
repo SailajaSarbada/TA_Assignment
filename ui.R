228ba19b77fe3071621c6b808c48456486f92a22
SailajaSarbada <sailajasarbada123@gmail.com>
2018-12-07 16:19:43 +0530

    Add files via upload

--- ui.R
+++ ui.R
@@ -4,16 +4,7 @@
 #---------------------------------------------------------------------#
 #               UDPipe NLP workflow App                               #
 #---------------------------------------------------------------------#
-install.packages("DT")
-library(udpipe)
-library(stringr)
-library("shiny")
-library(shinyWidgets)
-library(shinydashboard)
-library(igraph)
-library(ggraph)
-library(ggplot2)
-library(DT)
+
 shinyUI( fluidPage(
     
     titlePanel(" UDPipe NLP workflow"),
@@ -47,20 +38,20 @@ shinyUI( fluidPage(
                     tabPanel("Overview",
                              h4(p("Data input")),
                              p("This app supports only text data file.",align="justify"),
-                             p("Please refer to the link below for sample csv file."),
-                             a(href="https://github.com/sudhir-voleti/sample-data-sets/blob/master/Segmentation%20Discriminant%20and%20targeting%20data/ConneCtorPDASegmentation.csv"
-                               ,"Sample data input file"),   
                              br(),
                              h4('How to use this App'),
                              p('To use this app,'),
-                             p('Click on', strong("Upload text data"),'and upload the text data file.'),
-                             p('Click on',strong("Upload udpipe data"),'and upload the udpipe data file.'),
+                             p('Click on', strong("Upload text data"),'and upload the text data file.','Please refer to the link',
+                             a(href="https://raw.githubusercontent.com/SailajaSarbada/TA_Assignment/master/Sample%20Datasets/Nokia_Lumia_reviews.txt","Sample data input file"),' for sample text file.'
+                              ),
+                             p('Click on',strong("Upload udpipe data"),'and upload the udpipe data file.','Please refer to the link',
+                               a(href="https://github.com/SailajaSarbada/TA_Assignment/blob/master/Sample%20Datasets/english-ud-2.0-170801.udpipe","Sample udpipe data input file"),' for sample udpipe file.'),
                              p('Check',strong("Input checkbox"),'for XPOS selection.')
                     ),
                     tabPanel("Table",dataTableOutput('table'),
                              br(),br(),
-                             downloadButton('downloadData1', 'Download the data'),br(),br(),
-                             p(" write some story")),
+                             downloadButton('downloadData1', 'Download the data'),br(),br()
+                            ),
                     tabPanel("Co-occurrence",
                              plotOutput("plot")
                     )
