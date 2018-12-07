2b4fc00ef92d549963aa7a2716afe08f06098e99
SailajaSarbada <sailajasarbada123@gmail.com>
2018-12-07 15:34:02 +0530

    Add files via upload

---
+++ Libraries_support.R
@@ -0,0 +1,25 @@
+# setup code block
+try(require(shiny) || install.packages("shiny"))
+if (!require(udpipe)){install.packages("udpipe")}
+if (!require(stringr)){install.packages("stringr")}
+if (!require(lattice)){install.packages("lattice")}
+if (!require(igraph)){install.packages("igraph")}
+if (!require(ggraph)){install.packages("ggraph")}
+if (!require(ggplot2)){install.packages("ggplot2")}
+if (!require(shinyWidgets)){install.packages("shinyWidgets")}
+if (!require(shinydashboard)){install.packages("shinydashboard")}
+if (!require(DT)){install.packages("DT")}
+
+
+library(shiny)
+library(udpipe)
+library(textrank)
+library(lattice)
+library(igraph)
+library(ggraph)
+library(ggplot2)
+library(wordcloud)
+library(stringr)
+library(shinyWidgets)
+library(shinydashboard)
+library(DT)
